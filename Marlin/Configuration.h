20b2f59cd668a7ee31048bfdf198b8aba30314ca
jColfej <contact@colfej.fr>
2022-09-09 07:12:17 +0200

    Enable print stats
    Signed-off-by: jColfej <contact@colfej.fr>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2503,9 +2503,9 @@
  *
  * View the current statistics with M78.
  */
-//#define PRINTCOUNTER
+#define PRINTCOUNTER
 #if ENABLED(PRINTCOUNTER)
-#define PRINTCOUNTER_SAVE_INTERVAL 60 // (minutes) EEPROM save interval during print
+#define PRINTCOUNTER_SAVE_INTERVAL 10 // (minutes) EEPROM save interval during print
 #endif
 
 // @section security
