79fcf9f68ffb48c0a753114fd386f0d13035caaf
jColfej <contact@colfej.fr>
2023-01-05 07:38:50 +0100

    Fix home position
    Signed-off-by: jColfej <contact@colfej.fr>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2238,8 +2238,8 @@
 
 // Manually set the home position. Leave these undefined for automatic settings.
 // For DELTA this is the top-center of the Cartesian print volume.
-// #define MANUAL_X_HOME_POS 0
-// #define MANUAL_Y_HOME_POS 0
+#define MANUAL_X_HOME_POS 0
+#define MANUAL_Y_HOME_POS -11
 // #define MANUAL_Z_HOME_POS 0
 // #define MANUAL_I_HOME_POS 0
 // #define MANUAL_J_HOME_POS 0
