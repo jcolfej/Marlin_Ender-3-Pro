1a3be770a23c3f85aaf4c5edb60642747462ba1c
jColfej <contact@colfej.fr>
2023-01-05 07:38:27 +0100

    Change bed size to be compatible with Sprite Kit Pro
    Signed-off-by: jColfej <contact@colfej.fr>

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1853,8 +1853,8 @@
 // @section geometry
 
 // The size of the printable area
-#define X_BED_SIZE 235
-#define Y_BED_SIZE 235
+#define X_BED_SIZE 215
+#define Y_BED_SIZE 215
 
 // Travel limits (linear=mm, rotational=°) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
@@ -1862,7 +1862,7 @@
 #define Z_MIN_POS 0
 #define X_MAX_POS X_BED_SIZE
 #define Y_MAX_POS Y_BED_SIZE
-#define Z_MAX_POS 250
+#define Z_MAX_POS 220
 // #define I_MIN_POS 0
 // #define I_MAX_POS 50
 // #define J_MIN_POS 0
