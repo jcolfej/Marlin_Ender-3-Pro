7720cd7a0e74cb47ae34b7da3a97d52582144791
jColfej <contact@colfej.fr>
2024-06-23 15:26:42 +0200

    Fix CRTouch offset and connection

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1308,7 +1308,7 @@
 #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
 
 // Force the use of the probe for Z-axis homing
-//#define USE_PROBE_FOR_Z_HOMING
+#define USE_PROBE_FOR_Z_HOMING
 
 /**
  * Z_MIN_PROBE_PIN
@@ -1514,7 +1514,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -36.5, -40, 0 }
+#define NOZZLE_TO_PROBE_OFFSET { -32, -40, 0 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
